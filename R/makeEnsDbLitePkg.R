3d4bd94abe3889fa1e1e37895d06068d154fc19b
arcolombo <anthonycolombo60@gmail.com>
2015-08-28 21:44:23 +0000

    adding the README.md, and adding the email parameter for MAINTAINER of package. Has basic type checking for ASCII@ASCII.ASCII format.

--- R/makeEnsDbLitePkg.R
+++ R/makeEnsDbLitePkg.R
@@ -1,7 +1,8 @@
 #' create an EnsDbLite package from sqlite file (usually via ensDbLiteFromFasta)
 #'
 #' @param ensdblitefile   the sqlite filename
-#' @param author          whose fault this is (with email address)
+#' @param author          whose fault this is
+#' @param email           adding maintainer's email
 #' @param version         version of the package (default is "1.0")
 #' @param destDir         where to put the new package directory (".")
 #' 
@@ -9,13 +10,42 @@
 #' 
 #' @export
 #'
-makeEnsDbLitePkg <- function(ensdblitefile, author, version="1.0", destDir=".", ...){
+makeEnsDbLitePkg <- function(ensdblitefile, author,email, version="1.0", destDir=".", ...){
   
   stopifnot(class(ensdblitefile) == "character")
   ensdb <- EnsDbLite(x=ensdblitefile)
   md <- metadata(ensdb)
   fetchMeta <- function(x) md[x, "value"]
   pkg <- fetchMeta("package_name")
+ 
+   if (grepl("_",pkg)){
+   pkg<-gsub("_","",pkg)
+  }
+   if (grepl(".",pkg)){
+   pkg<-strsplit(pkg,split='.',fixed=TRUE)[[1]][1]
+  }
+
+if(missing(email)){
+   email<-"TommyTrojan@update.com"
+  
+}
+ #type checking the email parameter, and defaulting if it fails
+#weak regex for email additions
+ if(grepl("<",email)==TRUE || grepl(">",email)==TRUE) {
+    email<-gsub("<","",email)
+    email<-gsub(">","",email)
+ }  
+
+  if(grepl("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}",email)==TRUE){
+    message("email has format ASCII@ASCII.ACII ...")
+    email<-paste0("<",email)
+    email<-paste0(email,">")
+  
+   } 
+   if( grepl("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", email)==FALSE){
+    email<-"<TommyTrojan@update.com>"
+   }
+  maintainer<-paste(author,email,sep=" ")
   organism <- fetchMeta("organism")
   ensembl_version <- md["ensembl_version", "value"]
   template_path <- system.file("ensdblite", package="TxDbLite")
@@ -27,7 +57,7 @@ makeEnsDbLitePkg <- function(ensdblitefile, author, version="1.0", destDir=".", 
     PKGDESCRIPTION="Lightweight Ensembl transcript annotations",
     PKGVERSION=version,
     AUTHOR=author,
-    MAINTAINER=author,
+    MAINTAINER=maintainer,
     LIC="Artistic-2.0",
     ORGANISM=organism, 
     SPECIES=organism, 
