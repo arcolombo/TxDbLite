fff8eecc9e50890992365e3da1932316ee2567e7
ttriche <tim.triche@gmail.com>
2015-10-28 15:31:16 -0700

    work on findDupes demo

--- demo/findRepBaseDupes.R
+++ demo/findRepBaseDupes.R
@@ -2,5 +2,6 @@ library(TxDbLite)
 library(artemisData)
 
 fasta_dir <- system.file("extdata", "fasta", package="artemisData")
-repbase_files <- list.files(fasta_dir, pattern="^Homo_sapiens.RepBase.*.gz$")
+repbase_files <- list.files(fasta_dir, pattern="^Homo_sapiens.RepBase.*.fa$")
+repbase_files <- paste(fasta_dir, repbase_files, sep="/")
 findDupes(repbase_files)
