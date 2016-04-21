7dbaff39ef908deefb473ab52a7fa3f0afe32b53
ttriche <tim.triche@gmail.com>
2016-04-21 13:24:20 -0700

    add an example for mapToReactome()

--- R/mapToReactome.R
+++ R/mapToReactome.R
@@ -5,6 +5,8 @@
 #' 
 #' @return  a data.frame mapping IDs to Reactome IDs and pathways
 #' 
+#' @examples mapToReactome("ENST00000519619", type="transcript")
+#'
 #' @import  biomaRt 
 #' 
 #' @export
@@ -17,9 +19,10 @@ mapToReactome <- function(IDs, type=c("transcript", "gene")) {
                   transcript=c(ENST="ensembl_transcript_id",
                                HUGO="hgnc_transcript_name"))
 
+  message("Fetching mappings from biomart...")
+  message("(this may take a little while!)")
   ensembl <- useEnsembl(biomart="ensembl", 
                         dataset="hsapiens_gene_ensembl") 
-  message("Fetching mappings from biomart... this may take a while.")
   getBM(attributes=columns[[type]], filters=idtype, values=IDs, mart=ensembl)
 
 }
