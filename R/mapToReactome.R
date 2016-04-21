12e435cc1a804fb7609fa97b76a75a657bada05a
ttriche <tim.triche@gmail.com>
2016-04-21 13:20:49 -0700

    default mapToReactome() to transcript-level & bump version

--- R/mapToReactome.R
+++ R/mapToReactome.R
@@ -1,14 +1,14 @@
 #' map genes or transcripts to Reactome pathways 
 #'
 #' @param   IDs     the identifiers to map
-#' @param   type    what type of identifier are these? (gene)
+#' @param   type    what type of identifier are these? (transcript)
 #' 
 #' @return  a data.frame mapping IDs to Reactome IDs and pathways
 #' 
 #' @import  biomaRt 
 #' 
 #' @export
-mapToReactome <- function(IDs, type=c("gene","transcript")) { 
+mapToReactome <- function(IDs, type=c("transcript", "gene")) { 
 
   type <- match.arg(type) 
   idtype <- paste("ensembl", type, "id", sep="_") 
@@ -19,6 +19,7 @@ mapToReactome <- function(IDs, type=c("gene","transcript")) {
 
   ensembl <- useEnsembl(biomart="ensembl", 
                         dataset="hsapiens_gene_ensembl") 
+  message("Fetching mappings from biomart... this may take a while.")
   getBM(attributes=columns[[type]], filters=idtype, values=IDs, mart=ensembl)
 
 }
