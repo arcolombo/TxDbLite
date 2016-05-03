b7f6cd1647ad2f7f6f873989c7f99d2b2c262365
root <anthonycolombo60@gmail.com>
2016-05-03 16:46:25 -0700

    removing the bin directory not needed

--- inst/bin/extractAllLevels.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#for reactome, need to sub out , and ;  .
-
-awk '{gsub(/,/," ");print}' Ensembl2Reactome_All_Levels.txt | awk '{gsub(/;/," ");print}' | awk -F "\t" '{print $1"\t"$2"\t"$6"\t"$4}'  > reactomeAllLeveles.txt
-
-
-#read into csv and print out
