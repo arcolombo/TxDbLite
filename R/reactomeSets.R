ee3af012a41169802212513e66b1790745f4d410
root <anthonycolombo60@gmail.com>
2016-05-03 19:12:48 -0700

    adding reactomeSets to input a mappedReactome object which will take a mapped reactome set that intersects kexp to reactome  cache.. this allows for using the cache as well for details view : https://www.youtube.com/watch?v=gffDHJ15IhI

--- R/reactomeSets.R
+++ R/reactomeSets.R
@@ -2,16 +2,17 @@
 #'
 #' @param     species  what species are the IDs from? (default:Homo sapiens)
 #' @param     type     transcript- or gene-level mappings? (default: transcript)
-#' 
+#' @param     mappedReactome   list of the results output from mapToReactome, downstream to assemble the list into pathways. 
 #' @return    gene sets per Reactome term for this species 
 #' 
 #' @examples  reactomeSets("Homo sapiens") 
 #'
 #' @export
-reactomeSets <- function(species="Homo sapiens", type=c("transcript","gene")) {
-  
-  type <- match.arg(type)
-  abbreviation <- "HSA" # default
+reactomeSets <- function(species="Homo sapiens", type=c("transcript","gene"), mappedReactome=NULL) {
+ 
+ 
+   type <- match.arg(type)
+   abbreviation <- "HSA" # default
   if (species != "Homo sapiens") {
     abbreviations <- TxDbLite:::getSupportedAbbreviations("reactome")
     names(abbreviations) <- tolower(names(abbreviations))
@@ -20,10 +21,24 @@ reactomeSets <- function(species="Homo sapiens", type=c("transcript","gene")) {
     else abbreviation <- tolower(abbreviations[joined])
   }
   orgDetails <- getOrgDetails(species)
-  allTerms <- data.frame(term=unlist(reactomeCache[[abbreviation]]))
+   
+  if(is.null(mappedReactome)==FALSE) {
+     message(" creating reactome set from mapped reactome list...")
+  mp<-data.frame(term=unlist(mappedReactome))
+  mp$ID<-rownames(mp)
+  reactomeSet<-split(mp$ID,mp$term)
+   }
+
+  if(is.null(mappedReactome)==TRUE) { 
+  build<-getMostRecentCacheBuild()
+  reactomeCache<-getReactomeCache(species=species,build=build)
+  allTerms <- data.frame(term=unlist(reactomeCache[[toupper(abbreviation)]]))
   allTerms$ID <- rownames(allTerms)
   key <- with(orgDetails, switch(type, transcript=txpre, gene=gxpre))
   allTerms <- subset(allTerms, substr(allTerms$ID, 1, nchar(key)) == key)
-  split(allTerms$ID, allTerms$term)
+  reactomeSet<-split(allTerms$ID, allTerms$term)
+    }
+  message("done.\n")
+  return(reactomeSet)
 
 }
