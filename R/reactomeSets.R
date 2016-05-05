6d2e678cf0f5b4b3e3f8f04e1bde9f4fd22e9276
root <anthonycolombo60@gmail.com>
2016-05-04 17:35:30 -0700

    adding reactomeSets which corrects for the lengths of duplicates when creating the data frame.  we expect multiple gene entries per reacID, however when splitting by name (which is needed) R will add name.dupe1, name.dupe2  to the end.   the best way to do this is to find the original lengths, test for uniformity, then find the lengths of the names which changed after splitting, then substring out the duplicated suffices.  this is a bit horrendously technical... the alternative is creating matrices /DFs by scratch  but you have to be really stupid to do that.

--- R/reactomeSets.R
+++ R/reactomeSets.R
@@ -24,10 +24,31 @@ reactomeSets <- function(species="Homo sapiens", type=c("transcript","gene"), ma
    
   if(is.null(mappedReactome)==FALSE) {
      message(" creating reactome set from mapped reactome list...")
-  mp<-data.frame(term=unlist(mappedReactome))
-  mp$ID<-rownames(mp)
-  reactomeSet<-split(mp$ID,mp$term)
-   }
+     trueLengths<-nchar(names(mappedReactome))
+     mp<-data.frame(term=unlist(mappedReactome))
+     mp$ID<-rownames(mp)
+     #manually finding the lengths of nchars picked up from dupes
+      uniqueValue<-unique(trueLengths)
+      idx<-which(nchar(mp$ID)!=uniqueValue)
+     if(length(uniqueValue)>1) {
+      message("Dave, I'm afraid I found the transcript lengths to be non-uniform, and this causes problems with non-uniform ensembl transcrpit lengths...please input mapToReactome IDs with all same lengths")
+      stopifnot(length(uniqueValue)==1)
+      }
+     if(length(idx) > 0) { 
+      message("I detected duplicates...correcting...")
+      if(length(uniqueValue)==1) {
+      message("I split the reactomeSet, now correcting the ID lengths for duplicates...")
+      if( unique(nchar(mp$ID[idx ])) ==(uniqueValue+1)) {
+      mp$ID[idx]<-substr((mp$ID[idx]),1,uniqueValue)
+      rownames(mp)<-NULL
+       }  
+     }
+   } #if dupes are found when indexing
+   reactomeSet<-split(mp$ID,mp$term)
+   message("done\n")
+   return(reactomeSet[lapply(reactomeSet,length)>0])
+
+  } #if mappedReactome vector is input
 
   if(is.null(mappedReactome)==TRUE) { 
   build<-getMostRecentCacheBuild()
@@ -36,9 +57,33 @@ reactomeSets <- function(species="Homo sapiens", type=c("transcript","gene"), ma
   allTerms$ID<-rownames(allTerms)
   key <- with(orgDetails, switch(type, transcript=txpre, gene=gxpre))
   allTerms <- subset(allTerms, substr(allTerms$ID, 1, nchar(key)) == key)
-  reactomeSet<-split(allTerms$ID, allTerms$term)
-    }
-  message("done.\n")
-  return(reactomeSet)
+  
 
-}
+    keyedcacheNames<-subset(names(reactomeCache[[toupper(abbreviation)]]), substr(names(reactomeCache[[toupper(abbreviation)]]), 1, nchar(key)) == key)
+   trueLengths<-unique(nchar(keyedcacheNames))
+    message("checking if the cache ensembl names have uniform lengths ...")  
+   stopifnot(length(trueLengths)==1)
+    idx<-which(nchar(allTerms$ID)!=trueLengths)
+   
+
+    if(length(idx) > 0 ) { 
+     message("I detected duplicates, correcting the dupe names...")
+      if(length(trueLengths)==1) { 
+      message("I split the reactomeSet, now correcting the ID lengths for duplicates...")
+      if(unique(nchar(allTerms$ID[idx]))==(trueLengths+1)) {
+       allTerms$ID[idx]<-substr((allTerms$ID[idx]),1,trueLengths)
+       }
+    }
+ }
+  #since split was used there will exists dupe names which occur in multiple reactome IDs,  since we know the original lengths of the names, which have to scale back the R defaulted additions of name.dupe1  where "1" or "2" for second dupe ... is added to any dupe.  we can detect these and remove them.  
+   reactomeSet<-split(allTerms$ID, allTerms$term)
+   message("done\n")
+   return(reactomeSet[lapply(reactomeSet,length)>0])
+
+ 
+} 
+
+ 
+  
+
+} #{{{
