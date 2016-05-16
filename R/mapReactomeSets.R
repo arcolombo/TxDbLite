3a87113da157dc66f40a55999e428908ad814af4
root <anthonycolombo60@gmail.com>
2016-05-16 12:57:16 -0700

    starting the mapReactomeSets function for cloud revisions output

---
+++ R/mapReactomeSets.R
@@ -0,0 +1,2 @@
+#' this creates a reactomePathway set in terms of gene names from a reactomeSet which is a list in terms of reactomeId per ENSEMBL entry.   
+#' @return a list of reactomePathways per associated gene name
