61f037485fdb8a695ea162fffd71f6a8e29776e8
root <anthonycolombo60@gmail.com>
2016-05-16 17:58:56 -0700

    mapReactomeSets may not be used. but maps reactome sets to gene Sets

--- R/mapReactomeSets.R
+++ R/mapReactomeSets.R
@@ -1,2 +1,13 @@
 #' this creates a reactomePathway set in terms of gene names from a reactomeSet which is a list in terms of reactomeId per ENSEMBL entry.   
+#' @param reactomePathways  a cache rda object list with all the reactome Ids listing description of pathways
+#' @param reactomeSets  a list of reactome Ids per ENSEMBL gene / transcript set
 #' @return a list of reactomePathways per associated gene name
+
+mapReactomeSets<-function(reactomePathways, reactomeSets){
+
+indX<- names(reactomePathways) %in% tx.DF$pathway.name
+
+
+
+}#{{{main
+
